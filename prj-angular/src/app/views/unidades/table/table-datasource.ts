be3ffe29f1c5af7ad9c6d5f13983b141060a8584
RodrigoFrancaBR <rfrancacosta@gmail.com>
2020-07-07 23:36:39 -0300

    listando unidades

--- prj-angular/src/app/views/unidades/table/table-datasource.ts
+++ prj-angular/src/app/views/unidades/table/table-datasource.ts
@@ -1,3 +1,4 @@
+import { Unidade } from './../../../model/unidade';
 import { DataSource } from '@angular/cdk/collections';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -5,42 +6,42 @@ import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
 
 // TODO: Replace this with your own data model type
-export interface TableItem {
-  name: string;
-  id: number;
-}
+// export interface TableItem {
+//   name: string;
+//   id: number;
+// }
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: TableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
-];
+// const EXAMPLE_DATA: TableItem[] = [
+//   {id: 1, name: 'Hydrogen'},
+//   {id: 2, name: 'Helium'},
+//   {id: 3, name: 'Lithium'},
+//   {id: 4, name: 'Beryllium'},
+//   {id: 5, name: 'Boron'},
+//   {id: 6, name: 'Carbon'},
+//   {id: 7, name: 'Nitrogen'},
+//   {id: 8, name: 'Oxygen'},
+//   {id: 9, name: 'Fluorine'},
+//   {id: 10, name: 'Neon'},
+//   {id: 11, name: 'Sodium'},
+//   {id: 12, name: 'Magnesium'},
+//   {id: 13, name: 'Aluminum'},
+//   {id: 14, name: 'Silicon'},
+//   {id: 15, name: 'Phosphorus'},
+//   {id: 16, name: 'Sulfur'},
+//   {id: 17, name: 'Chlorine'},
+//   {id: 18, name: 'Argon'},
+//   {id: 19, name: 'Potassium'},
+//   {id: 20, name: 'Calcium'},
+// ];
 
 /**
  * Data source for the Table view. This class should
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class TableDataSource extends DataSource<TableItem> {
-  data: TableItem[] = EXAMPLE_DATA;
+export class TableDataSource extends DataSource<Unidade> {
+  data: Unidade[];
   paginator: MatPaginator;
   sort: MatSort;
 
@@ -53,7 +54,7 @@ export class TableDataSource extends DataSource<TableItem> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<TableItem[]> {
+  connect(): Observable<Unidade[]> {
     // Combine everything that affects the rendered data into one update
     // stream for the data-table to consume.
     const dataMutations = [
@@ -77,7 +78,7 @@ export class TableDataSource extends DataSource<TableItem> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: TableItem[]) {
+  private getPagedData(data: Unidade[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
   }
@@ -86,7 +87,7 @@ export class TableDataSource extends DataSource<TableItem> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: TableItem[]) {
+  private getSortedData(data: Unidade[]) {
     if (!this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -94,8 +95,8 @@ export class TableDataSource extends DataSource<TableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
+        case 'id': return compare(a.id, b.id, isAsc);
+        case 'nome': return compare(+a.nome, +b.nome, isAsc);
         default: return 0;
       }
     });
