172dcdcc01bedb9347ece6b558b02aa6be3ee121
RodrigoFrancaBR <rfrancacosta@gmail.com>
2020-07-11 22:35:58 -0300

    modal concluido

--- prj-angular/src/app/views/exemplo/exemplo.component.ts
+++ prj-angular/src/app/views/exemplo/exemplo.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ModalFoculsComponent } from 'src/app/components/modal/modal-foculs/modal-foculs.component';
 
 @Component({
   selector: 'app-exemplo',
   templateUrl: './exemplo.component.html',
+  // add NgbModalConfig and NgbModal to the component providers
+  providers: [NgbModalConfig, NgbModal],
   styleUrls: ['./exemplo.component.css']
 })
 export class ExemploComponent {
 
-  constructor(private modal: NgbModal) { }
+  constructor(config: NgbModalConfig, private modal: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   title = 'ng-bootstrap-modal-demo';
 
   condition = false;
 
-  open() {
-    this.modal.open(ModalFoculsComponent, { size: 'sm' });
+  open(content) {
+    this.modal.open(content, { size: 'sm' });
   }
 }
